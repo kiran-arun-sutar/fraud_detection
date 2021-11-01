78d044caab1cb7a20826f86ed1b8c709289f7d1b
kiran-arun-sutar <kiran.arun.sutar@gmail.com>
2021-11-01 07:53:03 +0530

    checking in ml models

--- model.py
+++ model.py
@@ -1,2 +1,12 @@
 
 print("Adding ML model code")
+
+def model1():
+    print("Addig code for model1 algorithms.")
+
+def model2():
+    print("Addig code for model2 algorithms.")
+    
+def model3():
+    print("Addig code for model3 algorithms.")
+    
